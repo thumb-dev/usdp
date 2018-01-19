cf509049724aaa95e75502f2300f5f296148e38c
thumb-dev <kevin@thumbprint.com>
2018-01-19 11:09:34 -0500

    Update navCtrl.js

--- app/js/controllers/navCtrl.js
+++ app/js/controllers/navCtrl.js
@@ -1,5 +1,25 @@
-four51.app.controller('NavCtrl', ['$location', '$route', '$scope', '$451', 'User',
-    function ($location, $route, $scope, $451, User) {
+four51.app.controller('NavCtrl', ['$location', '$route', '$scope', '$451', 'User', 'OrderSearchCriteria',
+    function ($location, $route, $scope, $451, User, OrderSearchCriteria) {
+      $scope.openOrderCount = 0;
+      OrderSearchCriteria.query(function(orders) {
+          angular.forEach(orders, function(order){
+              if(order.DisplayName == "Open"){
+                  $scope.openOrderCount = order.Count;
+              }
+          });
+      });
+
+      $scope.waitingOrderCount = 0;
+      OrderSearchCriteria.query(function(orders) {
+          angular.forEach(orders, function(order){
+              if(order.DisplayName == "Awaiting Approval"){
+                  $scope.waitingOrderCount = order.Count;
+              }
+          });
+      });
+      
+
+ 
         $scope.Logout = function(){
             User.logout();
             if ($scope.isAnon) {
