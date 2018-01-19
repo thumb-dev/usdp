60560ee408c0f79b79bde7d149548d2a9cf9e2cc
thumb-dev <kevin@thumbprint.com>
2018-01-19 11:42:46 -0500

    Create headerNavigation.js

---
+++ app/lib/oc/headerNavigation.js
@@ -0,0 +1,90 @@
+angular.module('OrderCloud-HeaderNavigation', []);
+angular.module('OrderCloud-HeaderNavigation')
+    .directive('headernavigation', headernavigation)
+;
+
+function headernavigation() {
+    return {
+        restrict: 'E',
+        template: template
+    };
+
+    function template() {
+        return [
+            '<style>',
+            '</style>',
+            '<section class="header-navigation">',
+            '<div class="row headerRow">',
+                '<div class="col-xs-12 col-sm-4 col-md-3 pull-left navImages">',
+                '<a ng-show="Four51User.isAuthenticated()" href="catalog">',
+                '<img class="headerLogo" src="{{user.Company.LogoUrl}}" />',
+                '</a>',
+                '</div>',
+                /*'<div class="col-xs-12 col-md-8 pull-right">',
+                '<ul class="pull-right">',
+                '<li ng-class="{\'active\': isActive([\'admin\', \'addresses\', \'address\', \'messages\', \'message\', \'favoriteorders\'])}">',
+                '<a href="admin" class="hidden-md hidden-lg">',
+                '<i class="fa fa-cog"></i>',
+                '<span class="hidden-xs">{{\'Account\' | r | xlat}}</span>',
+                '</a>',
+                '</li>',
+                '<li class="dropdown visible-md visible-lg" ng-class="{\'active\': isActive([\'admin\', \'addresses\', \'address\', \'messages\', \'message\', \'favoriteorders\'])}">',
+                '<a class="dropdown-toggle" data-toggle="dropdown">',
+                '<i class="fa fa-cog"></i>',
+                '<span class="hidden-xs">{{\'Account\' | r | xlat}}</span>',
+                '<b class="caret"></b>',
+                '</a>',
+                '<ul class="dropdown-menu">',
+                '<li ng-show="user.Permissions.contains(\'ViewSelfAdmin\')" class="admin">',
+                '<a href="admin">',
+                '{{\'User Information\' | r | xlat}}',
+                '</a>',
+                '</li>',
+                '<li ng-show="user.Type == \'Customer\' && (user.Permissions.contains(\'CreateShipToAddress\') || user.Permissions.contains(\'CreateBillToAddress\'))" class="addresses">',
+                '<a href="addresses">',
+                '{{\'Addresses\' | r | xlat}}',
+                '</a>',
+                '</li>',
+                '<li ng-show="user.Type == \'Customer\' && user.Permissions.contains(\'ViewMessaging\')" class="messages">',
+                '<a href="message">',
+                '{{\'Messages\' | r | xlat}}',
+                '</a>',
+                '</li>',
+                '<li ng-show="user.Permissions.contains(\'ViewContactUs\')" class="contactus">',
+                '<a href="contactus">',
+                '{{\'Contact Us\' | r | xlat}}',
+                '</a>',
+                '</li>',
+                '<li>',
+                '<a href="#" neworder ng-if="user.Permissions.contains(\'MultipleShoppingCart\') && currentOrder" class="neworder" ng-click="newOrderLoadingIndicator = true;startNewOrder()">',
+                '{{"Start" | r | xlat}} {{"New" | r | xlat}} {{"Order" | r | xlat}}',
+                '</a>',
+                '</li>',
+                '<li class="divider" ng-show="user.Type!=\'TempCustomer\' || AppConst.debug"></li>',
+                '<li ng-show="user.Type!=\'TempCustomer\'" class="logout">',
+                '<a href="#" ng-click="Logout()">',
+                '<i class="fa fa-power-off text-danger"></i>',
+                '<span>{{\'Log Out\' | r | xlat}}</span>',
+                '</a>',
+                '</li>',
+                '<li ng-if="AppConst.debug">',
+                '<a href="#" ng-click="Clear()">',
+                '<i class="fa fa-archive"></i>',
+                '<span class="text-nav">Clear Cache</span>',
+                '</a>',
+                '</li>',
+                '</ul>',
+                '</li>',
+                '</ul>',
+                '</div>',*/
+                '<div class="col-xs-12 col-sm-4 col-md-3 product-search">',
+                '<productsearchinput></productsearchinput>',
+                '</div>',
+                //'<div class="col-xs-12 col-sm-4 col-md-3 minicart-wrapper">',
+                //'<minicart></minicart>',
+                //'</div>',
+            '</div>',
+            '</section>'
+        ].join('');
+    }
+}
