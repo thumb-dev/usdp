6e22bd97554b1f453a42f692301631238d6a9302
thumb-dev <kevin@thumbprint.com>
2023-12-08 08:55:33 -0500

    Update contentModal7.js

--- app/lib/oc/contentModal7.js
+++ app/lib/oc/contentModal7.js
@@ -22,7 +22,7 @@ function contentmodal7() {
             // update the size of the modal window within the open()
             '<a ng-click="open7(500)" style="text-decoration: none;">',
             // replace the *Open Modal* below with your link name
-            '<span class="modalName PDT-modalName"></span> {{\'Watch Video\' | r | xlat}}',
+            '<span class="modalName PDT-modalName"></span> {{\'Open\' | r | xlat}}',
             '</a>'
         ].join('');
     }
@@ -54,16 +54,16 @@ function ContentModalCtrl7($scope, $modal) {
 
         function contentmodal7open() {
             return [
-                '<style>',
+               '<style>',
                 '.modal-header {min-height: 36px; padding: 2px;}',
                 '.modal-header h3 { margin-top:0;}',
                 '.modal-header h5 { font-size:1.16em; font-weight:bold; padding:5px 10px; text-shadow: 0 1px 0 #ffffff;}',
-                '.modal-header a.close {margin:0;padding:0;position:absolute;top:8px;right:150px;font-size:1.5em;color:#000;}',
+                '.modal-header a.close {margin:0;padding:0;position:absolute;top:8px;right:10px;font-size:1.5em;color:#000;}',
                 '.modal-body {width:100%; margin:0 auto; padding:10px 25px;}',
                 '</style>',
-                '<div class="modal-header">',
+                '<div class="modal-header" style="border-bottom: 1px solid #e5e5e5;margin-bottom: 20px;">',
                 //Optional title in top header
-                //'<h5 class="modal-title text-primary">Title</h5>',
+                '<h5 class="modal-title text-primary" style="text-align:center">Important Holiday Message from your team at Thumbprint</h5>',
                 //Optional close (x) in top header
                 '<a class="pull-right close" ng-click="close()">',
                 '<i class="fa fa-times"></i>',
@@ -73,25 +73,21 @@ function ContentModalCtrl7($scope, $modal) {
                 // '<h3>Heading</h3>',
                 //content block 1
                 // '<h4>Subheading</h4>',
-                // '<p>',
-                // 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,' +
-                // 'when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-                // '</p>',
+                '<p>',
+                'To let our team enjoy the holiday season, Thumbprint will be closed on <strong>Monday, December 25th, 2023 thru Monday, January 1st, 2024</strong>.<br/><br/>All orders placed during that time will be processed beginning Tuesday, January 2nd. We wish you a Happy Holidays and New Year!',
+                '</p>',
                 //content block 2
                 //'<h4>Subheading</h4>',
-                '<p class="videoWrapper">',
-                '<iframe width="560" height="315" src="https://player.vimeo.com/video/460599512?autoplay=1&loop=1&autopause=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-                '</p>',
                 '</div>',
                 //Optional footer
-                // '<div class="modal-footer">',
+                '<div class="modal-footer">',
                 // '<div class="pull-left">',
                 // '<a class="btn btn-default" ng-click="cancel()">Cancel</a>',
                 // '</div>',
-                // '<div class="pull-right">',
-                // '<a target="_blank" class="btn btn-primary " ng-click="close()">Close</a>',
-                // '</div>',
-                // '</div>'
+                '<div class="pull-right">',
+                '<a target="_blank" class="btn btn-primary " ng-click="close()">Close</a>',
+                '</div>',
+                '</div>'
             ].join('');
         }
 
@@ -114,5 +110,5 @@ function ContentModalCtrl7($scope, $modal) {
         };
 
     }];
-
+$scope.open6(500);
 }
