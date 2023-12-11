5f3af083f5815efb506491a78e8d60c41c952c06
thumb-dev <kevin@thumbprint.com>
2023-12-11 10:36:59 -0500

    Update contentModal7.js

--- app/lib/oc/contentModal7.js
+++ app/lib/oc/contentModal7.js
@@ -54,40 +54,56 @@ function ContentModalCtrl7($scope, $modal) {
 
         function contentmodal7open() {
             return [
-               '<style>',
-                '.modal-header {min-height: 36px; padding: 2px;}',
-                '.modal-header h3 { margin-top:0;}',
-                '.modal-header h5 { font-size:1.16em; font-weight:bold; padding:5px 10px; text-shadow: 0 1px 0 #ffffff;}',
-                '.modal-header a.close {margin:0;padding:0;position:absolute;top:8px;right:10px;font-size:1.5em;color:#000;}',
-                '.modal-body {width:100%; margin:0 auto; padding:10px 25px;}',
-                '</style>',
-                '<div class="modal-header" style="border-bottom: 1px solid #e5e5e5;margin-bottom: 20px;">',
-                //Optional title in top header
-                '<h5 class="modal-title text-primary" style="text-align:center">Important Holiday Message from your team at Thumbprint</h5>',
-                //Optional close (x) in top header
-                '<a class="pull-right close" ng-click="close()">',
-                '<i class="fa fa-times"></i>',
-                '</a>',
-                '</div>',
-                '<div class="modal-body">',
-                // '<h3>Heading</h3>',
-                //content block 1
-                // '<h4>Subheading</h4>',
-                '<p>',
-                'To let our team enjoy the holiday season, Thumbprint will be closed on <strong>Monday, December 25th, 2023 thru Monday, January 1st, 2024</strong>.<br/><br/>All orders placed during that time will be processed beginning Tuesday, January 2nd. We wish you a Happy Holidays and New Year!',
-                '</p>',
-                //content block 2
-                //'<h4>Subheading</h4>',
-                '</div>',
-                //Optional footer
-                '<div class="modal-footer">',
-                // '<div class="pull-left">',
-                // '<a class="btn btn-default" ng-click="cancel()">Cancel</a>',
-                // '</div>',
-                '<div class="pull-right">',
-                '<a target="_blank" class="btn btn-primary " ng-click="close()">Close</a>',
-                '</div>',
-                '</div>'
+                      '<style>',
+        '.modal-header {min-height: 36px; padding: 2px;border-bottom: 1px solid #e5e5e5;}',
+        '.modal-header h3 { margin-top:0;}',
+        '.modal-header h5 { font-size:1.16em; font-weight:bold; padding:5px 10px; text-shadow: 0 1px 0 #ffffff;}',
+        '.modal-header a.close {margin:0;padding:0;position:absolute;top:8px;right:10px;font-size:1.5em;color:#000;}',
+        '.modal-body {width:100%; margin:0 auto; padding:10px 25px;}',
+        '</style>',
+        '<div class="modal-header" style="padding:0;">',
+        //Optional title in top header
+        '<img src="https://thumbprint.com/four51_images/Our-offices-will-be-closed-December-24th-January-2nd_v1.png" alt="Happy Holidays" style="width: 100%;border-top-left-radius: 6px;border-top-right-radius: 6px;">',
+        // '<h5 class="modal-title text-primary">Happy Holiday from Thumbprint</i></h5>',
+        //Optional close (x) in top header
+        '<a class="pull-right close" ng-click="close()" style="top: 2px;">',
+        '<i class="fa fa-times"></i>',
+        '</a>',
+        '</div>',
+        '<div class="modal-body" style="padding-top: 25px;">',
+        // '<h3>Heading</h3>',
+        // content block 1
+        //'<h4>Subheading</h4>',
+        '<p>',
+        'Happy Holidays from all of us at Thumbprint!',
+        '</p>',
+        '<p>',
+        'To let our team enjoy the holiday season, Thumbprint will be closed on <strong>Monday, December 25th, 2023 thru Monday, January 1st, 2024</strong>. All orders placed during that time will be processed beginning Tuesday, January 2nd.',
+        '</p>',
+        '<p>',
+        'We wish you a Happy Holidays and New Year!',
+        '</p>',
+        //content block 2
+        // '<p>',
+        // '<div ng-repeat="field in user.CustomFields | filter:{Name:\'ADMI_Popup\'}">',
+        //   '<occheckboxfield id="update-password-field" customfield="field" ng-if="field.ControlType == \'Selection\'" ng-required="field.IsRequired" label=\'Check box to never see this pop-up again\' checked=\'Yes\' unchecked=\'No\'/>',
+        // '</div>',
+        // '</p>',
+        // <occheckboxfield customfield='Variant.Specs.Approved' label='Custom' checked='Yes' unchecked='No'></occheckboxfield>
+        //'<h4>Subheading</h4>',
+        '</div>',
+        //Optional footer
+        '<div class="modal-footer">',
+        '<div class="pull-left">',
+        // '<a class="btn btn-default" ng-click="cancel()">Cancel</a>',
+        // '<div ng-repeat="field in user.CustomFields | filter:{Name:\'Chewy_Popup\'}">',
+        // '<occheckboxfield id="update-password-field" customfield="field" ng-if="field.ControlType == \'Selection\'" ng-required="field.IsRequired" label=\'Check box to never see this pop-up again\' checked=\'Yes\' unchecked=\'No\'/>',
+        // '</div>',
+        '</div>',
+        '<div class="pull-right">',
+        '<a target="_blank" class="btn btn-info" ng-click="close()">Close</a>',
+        '</div>',
+        '</div>'
             ].join('');
         }
 
